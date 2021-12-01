f20cc0d9eabad11ca7504488acb0035556deb0d2
Jessica.W <jessica.watson1@live.com>
2021-11-30 20:32:41 -0700

    debugging with console log

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,11 +3,12 @@ const init = async () => {
   const response = await fetch('/.netlify/functions/signs')
   const data = await response.json()
 
-  document.querySelector('.container').innerHTML = `
-  <figure>
-    <img src="${data.url}" alt="Photo from instagram"
-    <figcaption>${data.caption}</figcaption>
-  </figure>
-  `
+  // document.querySelector('.container').innerHTML = `
+  // <figure>
+  //   <img src="${data.url}" alt="Photo from instagram"
+  //   <figcaption>${data.caption}</figcaption>
+  // </figure>
+  // `
+  console.log(data)
 }
 init()
